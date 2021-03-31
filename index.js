70db908617b9065c426384c611d8972fad58837b
Andrew Lee <andrewleee10@gmail.com>
2021-03-31 10:46:04 -0700

    complete code

--- index.js
+++ index.js
@@ -3,40 +3,11 @@ const ObjectId = require('mongojs').ObjectId
 const { prompt } = require('inquirer')
 const cTable = require('console.table')
 
-const mainMenu = () => {
-  prompt([
-    {
-      type: 'list',
-      name: 'main',
-      choices: ["View Books", "Add Book", "Update", "Delete Books", "Exit"],
-      message: "What would you like to do?"
-    }
-  ])
-    .then( res => {
-      switch(res.name) {
-        case "View Books": 
-          viewBooks()
-          break
-        case "Add Book":
-          addBook()
-          break
-        case "Update":
-          updatebook()
-          break
-        case "Delete Books":
-          deletebooks()
-          break
-        case "Exit":
-          process.exit()
-          break
-      }
-    })
-    .catch( err => console.log(err))
-}
 
 const viewBooks = () => {
   db.books.find((err, books) => {
     if (err) { console.log(err) }
+    books = books.map(({title, date, page}) => ({title, date, page}))
     console.table(books)
     mainMenu()
   })
@@ -60,10 +31,10 @@ const addBook = () => {
       message: "What page are you on currently?"
     }
   ])
-    .then(({title, date, page}) => {
-      db.books.insert({ title: `${title}`, dateStarted: `${date}`, page: `${page}`}, (err, book) => {
+    .then(book => {
+      db.books.insert((err, book) => {
         if (err) { console.log(err) }
-        console.table(book)
+        console.log('Book added successfully!')
         mainMenu()
       })
     })
@@ -71,68 +42,90 @@ const addBook = () => {
 }
 
 const updatebook = () => {
-  prompt ([
-    {
-      type: "input",
-      name: "title",
-      message: "Which book would you like to update?"
-    },
-    {
-      type: "list",
-      name: "update",
-      choices: ["Title", "Date Started", "Page Number"],
-      message: "What would you like to update?"
-    },
-    {
-      type: "input",
-      name: "new",
-      message: "What is the new info?"
-    }
-  ])
-    .then(res => {
-      switch(res.update) {
-        case "Title":
-          db.books.update({ title: `${res.title}`}, {$set: { title: `${res.new}`} }, (err, book) => {
-            if(err) { console.log(err) }
-            console.log("Changes made successfully!")
-            mainMenu()
-          })
-          break
-        case "Date Started":
-          db.books.update({ title: `${res.title}` }, { $set: { dateStarted: `${res.new}` } }, (err, book) => {
-            if (err) { console.log(err) }
-            console.log("Changes made successfully!")
-            mainMenu()
-          })
-          break
-        case "Page Number":
-          db.books.update({ title: `${res.title}` }, { $set: { page: `${res.new}` } }, (err, book) => {
-            if (err) { console.log(err) }
-            console.log("Changes made successfully!")
-            mainMenu()
-          })
-          break
+  db.books.find({}, (err, books) => {
+    if (err) { console.log(err) }
+
+    prompt ([
+      {
+        type: "list",
+        name: "_id",
+        choices: books.map(({_id, title}) => ({
+          name: title,
+          value: _id
+        })),
+        message: "Select a book"
+      },
+      {
+        type: "input",
+        name: "page",
+        message: "What page are you on?"
       }
-    })
-    .catch(err => console.log(err))
+    ])
+      .then(({ _id, page}) => {
+        db.books.update({ _id: ObjectId(_id)}, { $set: {page}}, err => {
+          if (err) { console.log(err) }
+          console.log("Book Successfully Updated!")
+          mainMenu()
+        })
+      })
+      .catch(err => console.log(err))
+  })
 }
 
 const deletebooks = () => {
-  prompt([
-    {
-      type: "input",
-      name: "title",
-      choices: "What is the title of the book you want to remove?"
-    }
-  ])
-    .then(({title}) => {
-      db.books.remove({ title: `${title}`}, (err, fields) => {
-        if (err) { console.log(err) }
-        console.log("Removed successfully.")
-        mainMenu()
+  db.books.find({}, (err, books) => {
+    if (err) { console.log(err) }
+
+    prompt([
+      {
+        type: "list",
+        name: "_id",
+        choices: books.map(({ _id, title }) => ({
+          name: title,
+          value: _id
+        })),
+        message: "Select a book"
+      }
+    ])
+      .then(({ _id, page }) => {
+        db.books.remove({ _id: ObjectId(_id) }, err => {
+          if (err) { console.log(err) }
+          console.log("Book Successfully Removed!")
+          mainMenu()
+        })
       })
-    })
-    .catch(err => console.log(err))
+      .catch(err => console.log(err))
+  })
 }
 
+const mainMenu = () => {
+  prompt([
+    {
+      type: 'list',
+      name: 'main',
+      choices: ["View Books", "Add Book", "Update", "Delete Books", "Exit"],
+      message: "What would you like to do?"
+    }
+  ])
+    .then( ({main}) => {
+      switch(main) {
+        case "View Books": 
+          viewBooks()
+          break
+        case "Add Book":
+          addBook()
+          break
+        case "Update":
+          updatebook()
+          break
+        case "Delete Books":
+          deletebooks()
+          break
+        case "Exit":
+          process.exit()
+          break
+      }
+    })
+    .catch( err => console.log(err))
+}
 mainMenu()
